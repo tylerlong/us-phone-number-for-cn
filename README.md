73bb893ff43cbfa3b3e7edba0220e39d98aa741f
Tyler Long <tyler4long@gmail.com>
2021-07-24 19:11:00 -0700

    Try to send sms

---
+++ README.md
@@ -0,0 +1,16 @@
+# us-phone-number-for-cn
+
+```
+Response:
+    {
+  "data": {
+    "errorCode": "InternationalProhibited",
+    "message": "International SMS is not available for account.",
+    "errors": [
+      {
+        "errorCode": "MSG-240",
+        "message": "International SMS is not available for account."
+      }
+    ]
+  }
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import PubNubExtension from '@rc-ex/pubnub';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+const pubNubExtension = new PubNubExtension();
+
+(async () => {
+  await rc.installExtension(pubNubExtension);
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  const r = await rc.restapi().account().extension().phoneNumber().get();
+  const smsPhoneNumbers = r.records?.filter(
+    p => p.features?.indexOf('SmsSender') !== -1
+  );
+  if (smsPhoneNumbers?.length === 0) {
+    console.error('This is no number available to send SMS');
+    return;
+  }
+  console.log(JSON.stringify(r, null, 2));
+  const r2 = await rc
+    .restapi()
+    .account()
+    .extension()
+    .sms()
+    .post({
+      from: {
+        phoneNumber: smsPhoneNumbers![0].phoneNumber,
+      },
+      to: [
+        {
+          phoneNumber: process.env.RINGCENTRAL_RECEIVER,
+        },
+      ],
+      text: 'Hello world!',
+    });
+  console.log(JSON.stringify(r2, null, 2));
+})();
